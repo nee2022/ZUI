2aa35fa354cbca0fd4f988d939012bdbc4d606a9
zhangyihua <642850742@qq.com>
2023-03-01 16:52:29 +0800

    webpack运行测试

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './lib/index.tsx'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist/lib'),
+        library: 'ZUI',
+        libraryTarget: 'umd',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        })
+    ]
+}
